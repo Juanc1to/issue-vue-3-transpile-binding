81bd61604d4512ce238e62daa6d0e76ac45d55a5
John L. Clark <jlc6@po.cwru.edu>
2021-01-20 18:12:28 -0500

    Add the code demonstrating the issue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,50 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <button @click="add()">Add an item</button>
+    <table>
+      <tr v-for="(item, index) in list" :key="index">
+        <td>
+          <form @submit.prevent="generate(index)">
+            <span>What's your guess?</span>
+            <select v-model="item.guess">
+              <option disabled :value="undefined">Select one:</option>
+              <option :value="true">Heads</option>
+              <option :value="false">Tails</option>
+            </select>
+            <button v-if="item.reveal === false" type="submit"
+              >Check</button>
+            <span v-else-if="item.heads === item.guess"
+              >Correct ({{ item.heads }})!</span>
+            <span v-else>Alas, it's actually
+            {{ item.heads === true ? 'Heads' : 'Tails' }}.</span>
+          </form>
+        </td>
+      </tr>
+    </table>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      list: [],
+    };
+  },
+  methods: {
+    add() {
+      this.list.push({
+        heads: undefined,
+        guess: true,
+        reveal: false,
+      });
+    },
+    generate(index) {
+      const value = Math.floor(Math.random() * 2);
+      this.list[index].heads = [true, false][value];
+      this.list[index].reveal = true;
+    },
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
